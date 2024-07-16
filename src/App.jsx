4e38f0ba8841c03ef7af0fcdb9f379f68af87d45
Thobani660 <zondit660@gmail.com>
2024-07-16 15:56:20 +0200

    latestup

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Footer from "./components/section/footer";
 
 function App() {
 const [oranges, setOranges] = useState(["Anne","Tucker"]);
+let scoopy = "Dooo";
+const list = [1,2,3];
+const pmb = ["riverside", "indumiso"]
 
-
-
+console.log(list ,"ect")
 console.log("I pulled and now I push.")
 
   console.log("Why so serious..........")
@@ -18,14 +20,16 @@ console.log("I pulled and now I push.")
   console.log("Always git push")
 
   console.log("why the bicycle cant stand on it own??, its too tired")
-
-
   console.log("Watermelon")
+  console.log(pmb)
+  console.log({pmb})
 
   return (
     <>
-      <h1>My name is {oranges[0]}</h1>
-      <section><Footer/></section>
+      <h1 >My name is {oranges[0]}</h1>
+      <p id={list}>props pros</p>
+      <p id={pmb}>my locaton {pmb[0]}</p>
+      <footer id={scoopy}><Footer/></footer>
     </>
   );
 }
