f47b066dc7e95981b9e6382c64613e76ee8d70a1
User <User@WRSD-L309-027.dut.ac.za>
2024-07-16 15:03:07 +0200

    new comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Logo from './components/logo/logo'
 
 function App() {
 const [oranges, setOranges] = useState(["Anne","Tucker"]);
@@ -24,6 +25,9 @@ console.log("I pulled and now I push.")
   return (
     <>
       <h1>My name is {oranges[0]}</h1>
+
+      <Logo/>
+
     </>
   );
 }
---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,11 @@
+import Logo from '../src/assets/logo.webp'
+
+const logo =() =>{
+return (
+    <>
+    <img src= alt="" />
+    </>
+)
+}
+
+export default logo
