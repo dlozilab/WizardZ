a1af4a2b6ca698a446b7d8c2330240842bcc4b5a
Nokwanda2000 <nokwandachonco262@gmail.com>
2024-07-16 15:58:46 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import Image from "./components/image/img";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
 function App() {
 const [oranges, setOranges] = useState(["Anne","Tucker"]);
+
+let  profile = "Show a picture"
+let List = [1,2,3]
+
 
 
 
@@ -20,10 +27,18 @@ console.log("I pulled and now I push.")
 
 
   console.log("Watermelon")
+  
+  
 
   return (
     <>
-      <h1>My name is {oranges[0]}</h1>
+     
+      <Image  id={profile}></Image> 
+      
+
+      
+
+    
     </>
   );
 }
---
+++ src/components/image/img.jsx
@@ -0,0 +1,14 @@
+import Imojie from "../image/imojie.jpg";
+
+
+function Image({id}){
+
+
+    console.log(id)
+    return(
+    <img id="Imojie" src={Imojie}></img>
+    )
+
+}
+
+export default Image;
