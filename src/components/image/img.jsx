f437d113e63fbf124cb6a8a752c02f87000dd489
SANEH2015 <mkhizes834@gmail.com>
2024-07-17 11:18:59 +0200

    button

--- src/components/image/img.jsx
+++ src/components/image/img.jsx
@@ -4,8 +4,8 @@ import Imojie from "../image/imojie.jpg";
 function Image({id,list}){
 
 
-    console.log(id)
-    console.log(list)
+     console.log(id)
+     console.log(list)
     return(
     <img id="Imojie" src={Imojie}></img>
     )
